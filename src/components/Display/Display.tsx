ec471e295e22f007681adaef44a2f139b6bcd5c4
nazmus767921 <nazmus767921@gmail.com>
2023-10-15 18:48:19 +0600

    updated

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -17,28 +17,31 @@ const Display = (): React.JSX.Element => {
 
   return (
     <Section>
-      {calculatedValues.map((c) => {
-        const { slope, yInterceptor, fontMax, fontMin, widthMin, widthMax } = c;
-        return (
-          <div className="syntax">
-            <SyntaxHighlighter
-              language="css"
-              style={materialDark}
-              showLineNumbers
-              customStyle={codeHighlighterStyle}
-            >
-              {`@media only screen and (min-width: ${widthMin}px and max-width: ${widthMax}px) {
-    body {
+      <div className="container">
+        {calculatedValues.map((c) => {
+          const { slope, yInterceptor, fontMax, fontMin, widthMin, widthMax } =
+            c;
+          return (
+            <div className="syntax">
+              <SyntaxHighlighter
+                language="css"
+                style={materialDark}
+                showLineNumbers
+                customStyle={codeHighlighterStyle}
+              >
+                {`@media screen and (min-width: ${widthMin}px) and (max-width: ${widthMax}px) {
+    html {
       font-size: 1rem; /* browser support fallback */
       font-size: clamp(${getRem(fontMin)}, ${(slope * 100).toFixed(5)}vw + ${(
-                yInterceptor / 16
-              ).toFixed(5)}rem, ${fontMax / 16}rem);
+                  yInterceptor / 16
+                ).toFixed(5)}rem, ${fontMax / 16}rem);
     }
   };`}
-            </SyntaxHighlighter>
-          </div>
-        );
-      })}
+              </SyntaxHighlighter>
+            </div>
+          );
+        })}
+      </div>
     </Section>
   );
 };
@@ -46,24 +49,34 @@ const Display = (): React.JSX.Element => {
 const Section = styled.div`
   grid-area: display;
 
-  /* max-width: 50em; */
   display: flex;
-  flex-direction: column;
-  gap: 1.25em;
-  align-items: center;
-  justify-content: flex-start;
+  align-items: flex-start;
+  justify-content: center;
 
-  padding-block: 5em;
+  position: relative;
+
+  padding-top: 2em;
   padding-inline: 2em;
 
   background-color: ${(props) => props.theme["bg-syntax"]};
   .syntax {
     font-size: 1em;
+    padding-block: 1em;
   }
   @media screen and (min-width: 1280px) {
+  padding-top: 12em;
+
     .syntax {
       font-size: 0.8em;
     }
+  }
+  .container {
+    position: sticky;
+    top: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
   }
 `;
 
