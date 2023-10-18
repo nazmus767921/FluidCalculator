8079817d03a9c9af617fd66696d1fd0b4cfc8d76
nazmus767921 <nazmus767921@gmail.com>
2023-10-18 18:18:57 +0600

    updated

--- src/pages/Documentation/components/styledElements.tsx
+++ src/pages/Documentation/components/styledElements.tsx
@@ -2,11 +2,12 @@ import styled from "styled-components";
 import { border } from "../../../styles";
 
 export const H1 = styled.h1`
+  width: 100%;
   font-size: 1.5em;
   color: ${(props) => props.theme.text};
   padding-block: 0.5em;
 
   margin-block: 0.5em;
 
-  border-bottom: ${border["--border-sm-solid"]} rgba(27, 22, 66, 0.1);
+  border-bottom: ${border["--border-sm-solid"]} rgba(27, 22, 66, 0.166);
 `;
